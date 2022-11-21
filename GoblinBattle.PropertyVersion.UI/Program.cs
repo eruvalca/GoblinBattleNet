8aab7ceec776a254a546d3eba5e568f4f258d86e
eruvalca <eruvalca@outlook.com>
2022-11-21 16:34:52 -0500

    Add project files.

---
+++ GoblinBattle.PropertyVersion.UI/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using GoblinBattle.PropertyVersion.UI;
+
+Goblin g1 = new();
+Goblin g2 = new();
+
+g1.HitPoints = 10;
+g1.Name = "Bob";
+
+g2.HitPoints = 10;
+g2.Name = "Tom";
+
+// they should take turns attacking, goblin 1 will go first
+int whoseTurn = 1;
+
+while (!g1.IsDead && !g2.IsDead)
+{
+    if (whoseTurn == 1)
+    {
+        g1.Attack(g2);
+        whoseTurn = 2;
+    }
+    else
+    {
+        g2.Attack(g1);
+        whoseTurn = 1;
+    }
+}
+
+Console.WriteLine("The battle is ended!");
+Console.ReadLine();
---
+++ GoblinBattleNet/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using GoblinBattle.MethodVersion.UI;
+
+Goblin g1 = new();
+Goblin g2 = new();
+
+g1.SetName("Bob");
+g1.SetHitPoints(10);
+
+g2.SetName("Tom");
+g2.SetHitPoints(10);
+
+// they should take turns attacking, goblin 1 will go first
+int whoseTurn = 1;
+
+while (!g1.GetIsDead() && !g2.GetIsDead())
+{
+    if (whoseTurn == 1)
+    {
+        g1.Attack(g2);
+        whoseTurn = 2;
+    }
+    else
+    {
+        g2.Attack(g1);
+        whoseTurn = 1;
+    }
+}
+
+Console.WriteLine("The battle is ended!");
+Console.ReadLine();
